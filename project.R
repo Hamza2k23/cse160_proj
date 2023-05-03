89b3e1363bf9986148410b4a24518fcfc2481834
ard223 <ard223@lehigh.edu>
2023-05-03 16:10:07 -0400

    RMSE KFold Model

--- project.R
+++ project.R
@@ -2,6 +2,7 @@ library(ggplot2)
 library(caret)
 source("data.R")
 source("split.R")
+library(iris)
 
 ## R Markdown
 
@@ -14,18 +15,25 @@ background$relationship_m
 background$explain_wrong_m
 
 split_data <- get_split_data(background)
-nrow(background$gpa)
-train <- split_data[1]
-test <- split_data[2]
-train_control <- split_data[3] # to be used when building model. refer to use for how use  [https://www.geeksforgeeks.org/k-fold-cross-validation-in-r-programming/#]
 
-model <- train(gpa~ miss_m + miss_f + time_lived_m + time_lived_f + closeness_m + closeness_f + verbal_abuse_o +
+train <- data.frame(split_data[1])
+test <- data.frame(split_data[2])
+
+# to be used when building model. refer to use for how use  [https://www.geeksforgeeks.org/k-fold-cross-validation-in-r-programming/#]
+
+
+
+model <- train(gpa~ miss_events_m + miss_events_f + time_lived_m + time_lived_f + closeness_m + closeness_f + verbal_abuse_o +
                  emotional_exp_o + verbal_abuse_mf + verbal_abuse_f + verbal_abuse_m + treats_other_child_f +
-                 treats_other_child_m + explain_wrong_f + explain_wrong_m + relationship_f + relationship_m + household_income, data = background,
-               trControl = train_control,
-               method = "nb")
+                 treats_other_child_m + explain_wrong_f + explain_wrong_m + relationship_f + relationship_m + household_income, data = train,
+               trControl = trainControl(method = "cv", number = 10), method = "lm")
+
 
 pred = predict(model, test, type="raw")
-tab <- table(pred, testData$class)
+pred
+tab <- table(pred, test$gpa)
+tab
+rmse <- sqrt(mean((pred - test$gpa)^2))
+rmse
 accuracy <- sum(diag(tab))/sum(tab)
 accuracy
