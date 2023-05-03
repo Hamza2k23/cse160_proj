a101677ab379ebed436c97d954c73ff83407f81d
MaxJikharev <thetaspiration@gmail.com>
2023-05-02 20:44:12 -0400

    fix split, implement in project.rmd

--- split.R
+++ split.R
@@ -1,11 +1,13 @@
 # Take dataframe, split using K-Fold, & return test/train pair.
+library("caret")
+
 get_split_data <- function(dataframe) {
     set.seed(232)
-    rand <- sample(1:nrow(background),nrow(background)*0.8, replace = FALSE) # 
-    train <- background[rand, ]
-    test <- background[-rand, ] #selects the other 0.2 indices
+    rand <- sample(1:nrow(dataframe),nrow(dataframe)*0.8, replace = FALSE) # 
+    train <- dataframe[rand, ]
+    test <- dataframe[-rand, ] #selects the other 0.2 indices
 
     train_control <- trainControl(method="cv", number=10) 
 
-    return(train, test, train_control)
+    return(list(train, test, train_control))
 }
