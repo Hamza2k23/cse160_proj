ebb6dfff91c655f85c31fe89a8bc4cfa20585411
ard223 <ard223@lehigh.edu>
2023-05-02 20:52:12 -0400

    project.R

--- project.R
+++ project.R
@@ -1,17 +1,31 @@
+library(ggplot2)
 library(caret)
-
+source("data.R")
+source("split.R")
 
 ## R Markdown
 
 # retrieves cleaned data with merged gpa 
+
 background <- get_data()
 
-split_data <- split_data(background)
+background$household_income
+background$relationship_m
+background$explain_wrong_m
 
-train <- split_data$train
-test <- split_data$test
-train_control <- split_data$train_control # to be used when building model. refer to use for how use  [https://www.geeksforgeeks.org/k-fold-cross-validation-in-r-programming/#]
+split_data <- get_split_data(background)
+nrow(background$gpa)
+train <- split_data[1]
+test <- split_data[2]
+train_control <- split_data[3] # to be used when building model. refer to use for how use  [https://www.geeksforgeeks.org/k-fold-cross-validation-in-r-programming/#]
 
+model <- train(gpa~ miss_m + miss_f + time_lived_m + time_lived_f + closeness_m + closeness_f + verbal_abuse_o +
+                 emotional_exp_o + verbal_abuse_mf + verbal_abuse_f + verbal_abuse_m + treats_other_child_f +
+                 treats_other_child_m + explain_wrong_f + explain_wrong_m + relationship_f + relationship_m + household_income, data = background,
+               trControl = train_control,
+               method = "nb")
 
-
-
+pred = predict(model, test, type="raw")
+tab <- table(pred, testData$class)
+accuracy <- sum(diag(tab))/sum(tab)
+accuracy
